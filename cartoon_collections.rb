9c1bcd70f8cedfbadfd4f21161f2d53630306200
Marcus Jun <mjun@calstate.edu>
2018-04-06 21:20:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(planeteer)# code an argument here
   # Your code here
-  planeteer.map do |word|
+  planeteer.collect do |word|
     word.capitalize << "!"
   end
 end
