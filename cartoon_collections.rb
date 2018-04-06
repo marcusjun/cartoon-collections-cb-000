3e2c61b76b23079694cbea34eb64d1a04032d3ab
Marcus Jun <mjun@calstate.edu>
2018-04-06 21:17:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,13 @@ def long_planeteer_calls(array)# code an argument here
 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |ingredient|
+    if array.include(ingredient)
+      return ingredient
+    end
+  end
+  nil
 end
