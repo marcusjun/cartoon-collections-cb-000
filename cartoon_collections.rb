23603985d5e633709a1a98635c960ccbaf84e3d0
Marcus Jun <mjun@calstate.edu>
2018-04-06 21:10:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.
-  
+  array.any?{|word| word.length >4 }
+
 end
 
 def find_the_cheese# code an argument here
